f103fd32c5e2aa6c3cb7e141bc7d0a48c989330f
owomed <owomedsunucu@gmail.com>
2025-08-02 22:52:13 +0300

    jail.js

--- commands/jail.js
+++ commands/jail.js
@@ -8,20 +8,20 @@ module.exports = {
     aliases: [],
     async execute(client, message, args) {
         if (!message.member.hasPermission('MANAGE_ROLES') && !message.member.roles.cache.some(role => id.Jail.jailyetkiliid.includes(role.id)) && message.author.id !== ayar.sahip) {
-            return message.lineReply('`Bu komudu kullanmak için gerekli izinlere sahip değilsin!`').then(x => x.delete({ timeout: 3000 }), message.react(id.Emojiler.başarısızemojiid));
+            return message.reply('`Bu komudu kullanmak için gerekli izinlere sahip değilsin!`').then(x => x.delete({ timeout: 3000 }), message.react(id.Emojiler.başarısızemojiid));
         }
 
         let üye = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
         let sebep = args.slice(1).join(' ');
 
         if (!üye || !sebep) {
-            return message.lineReply('`Jaile atabilmek için üye ve sebep belirtmelisin!`').then(x => x.delete({ timeout: 3000 }));
+            return message.reply('`Jaile atabilmek için üye ve sebep belirtmelisin!`').then(x => x.delete({ timeout: 3000 }));
         }
         if (üye.roles.cache.has(id.Jail.jailrolid)) {
-            return message.lineReply('`Etiketlenen üye zaten jailde!`').then(x => x.delete({ timeout: 3000 }));
+            return message.reply('`Etiketlenen üye zaten jailde!`').then(x => x.delete({ timeout: 3000 }));
         }
         if (message.member.roles.highest.position <= üye.roles.highest.position) {
-            return message.lineReply('`Etiketlediğin kullanıcı senden üst veya senle aynı pozisyonda!`').then(x => x.delete({ timeout: 3000 }));
+            return message.reply('`Etiketlediğin kullanıcı senden üst veya senle aynı pozisyonda!`').then(x => x.delete({ timeout: 3000 }));
         }
 
         let roller = üye.roles.cache;
@@ -44,7 +44,7 @@ module.exports = {
             await üye.roles.set([id.Jail.jailrolid]); // Jail rolünü ekle, diğer tüm rolleri kaldır
             
             // Kanala mesaj gönder
-            await message.lineReply(`\`${üye.user.tag}\` başarıyla jaile atıldı!`).then(x => x.delete({ timeout: 9000 }), message.react(id.Emojiler.başarılıemojiid));
+            await message.reply(`\`${üye.user.tag}\` başarıyla jaile atıldı!`).then(x => x.delete({ timeout: 9000 }), message.react(id.Emojiler.başarılıemojiid));
             
             // Kanala embed mesaj gönder
             await message.channel.send({ embed: embed });
@@ -58,7 +58,7 @@ module.exports = {
             }
         } catch (error) {
             console.error('Jail işlemi sırasında bir hata oluştu:', error);
-            return message.lineReply('`Jail işlemi sırasında bir hata oluştu.`').then(x => x.delete({ timeout: 3000 }));
+            return message.reply('`Jail işlemi sırasında bir hata oluştu.`').then(x => x.delete({ timeout: 3000 }));
         }
     }
 };
