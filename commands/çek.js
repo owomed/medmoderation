4922fa623f6bb2e9280da0b4c48b20b4f4886b65
owomed <owomedsunucu@gmail.com>
2025-08-02 23:14:19 +0300

    çek.js

--- commands/çek.js
+++ commands/çek.js
@@ -12,9 +12,9 @@ module.exports = {
     async execute(client, message, args) {
 
         let üye = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
-        if (!üye) return message.lineReply("`Çekebilmek için bir üye belirtmelisin!`").then(x => x.delete({ timeout: 3000 }));
-        if (!message.member.voice.channel || !üye.voice.channel || message.member.voice.channelID == üye.voice.channelID) return message.lineReply('`Etiketlenen üye veya sen seste bulunmamaktasın!`').then(x => x.delete({ timeout: 5000 }));
-        if (message.member.hasPermission("ADMINISTRATOR")) { await üye.voice.setChannel(message.member.voice.channelID), message.lineReply('`Üye başarılı bir şekilde bulunduğun odaya çekildi.`').then(x => x.delete({ timeout: 7000 }), message.react(id.Emojiler.başarılıemojiid)); } else {
+        if (!üye) return message.reply("`Çekebilmek için bir üye belirtmelisin!`").then(x => x.delete({ timeout: 3000 }));
+        if (!message.member.voice.channel || !üye.voice.channel || message.member.voice.channelID == üye.voice.channelID) return message.reply('`Etiketlenen üye veya sen seste bulunmamaktasın!`').then(x => x.delete({ timeout: 5000 }));
+        if (message.member.hasPermission("ADMINISTRATOR")) { await üye.voice.setChannel(message.member.voice.channelID), message.reply('`Üye başarılı bir şekilde bulunduğun odaya çekildi.`').then(x => x.delete({ timeout: 7000 }), message.react(id.Emojiler.başarılıemojiid)); } else {
             const reactionFilter = (reaction, user) => { return ['✅'].includes(reaction.emoji.name) && user.id === üye.id };
             message.channel.send(new Discord.MessageEmbed().setDescription(`${üye}, ${message.author} seni yanına çekmek istiyor. Gitmek istiyor musun?`).setFooter('Onaylamak için 15 saniyen var.')).then(async msj => {
                 await msj.react('✅');
@@ -23,7 +23,7 @@ module.exports = {
                     let onay = c.first();
                     if (onay) {
                         üye.voice.setChannel(message.member.voice.channelID);
-                        msj.delete(), message.lineReply('`Üye başarılı bir şekilde bulunduğun odaya çekildi.`').then(x => x.delete({ timeout: 7000 }), message.react(id.Emojiler.başarılıemojiid));
+                        msj.delete(), message.reply('`Üye başarılı bir şekilde bulunduğun odaya çekildi.`').then(x => x.delete({ timeout: 7000 }), message.react(id.Emojiler.başarılıemojiid));
                     };
                 });
             });
