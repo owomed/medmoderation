1e3f029b8db1af901d22d635799393087ec19f5a
owomed <owomedsunucu@gmail.com>
2025-08-02 23:13:24 +0300

    vunmute.js

--- commands/vunmute.js
+++ commands/vunmute.js
@@ -14,12 +14,12 @@ module.exports = {
 
         let üye = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
 
-        if (!üye) return message.lineReply('`Ses kanallarında susturulmasını kaldırmak için bir kullanıcı belirtmelisin!`').then(x => x.delete({ timeout: 3000 }));
-        if (!üye.voice.channel) return message.lineReply('`Üye ses kanalında bulunmamaktadır!`').then(x => x.delete({ timeout: 3000 }));
-        if (message.member.roles.highest.position <= üye.roles.highest.position) return message.lineReply('`Etiketlediğin kullanıcı senden üst veya senle aynı pozisyonda!`').then(x => x.delete({ timeout: 3000 }))
+        if (!üye) return message.reply('`Ses kanallarında susturulmasını kaldırmak için bir kullanıcı belirtmelisin!`').then(x => x.delete({ timeout: 3000 }));
+        if (!üye.voice.channel) return message.reply('`Üye ses kanalında bulunmamaktadır!`').then(x => x.delete({ timeout: 3000 }));
+        if (message.member.roles.highest.position <= üye.roles.highest.position) return message.reply('`Etiketlediğin kullanıcı senden üst veya senle aynı pozisyonda!`').then(x => x.delete({ timeout: 3000 }))
 
         client.channels.cache.get(id.Mute.mutelogkanalid).send(new Discord.MessageEmbed().setColor('#97ffff').setDescription(`${üye}\`(${üye.id})\` adlı üye, <@${message.author.id}>\`(${message.author.id})\` üyesi tarafından \`(${new Date().toTurkishFormatDate()})\` zamanında ses kanallarında ki susturulması kaldırdı.`))
 
-        üye.voice.setMute(false), message.lineReply('`Etiketlenen üyenin başarıyla ses kanallarında ki susturulması kaldırıldı!`').then(x => x.delete({ timeout: 9000 }), message.react(id.Emojiler.başarılıemojiid))
+        üye.voice.setMute(false), message.reply('`Etiketlenen üyenin başarıyla ses kanallarında ki susturulması kaldırıldı!`').then(x => x.delete({ timeout: 9000 }), message.react(id.Emojiler.başarılıemojiid))
     }
 }
