ca0e697466f11905baa1ecc348d6ef36c000e743
owomed <owomedsunucu@gmail.com>
2025-08-02 23:10:30 +0300

    temp-vmute.js

--- commands/temp-vmute.js
+++ commands/temp-vmute.js
@@ -17,13 +17,13 @@ module.exports = {
         let sebep = args.slice(2).join(' ');
         let süre = args[1];
         if (!üye || !süre || !sebep) return message.lineReply('`Ses kanallarında susturmak için üye, süre(1s,1m,1h,1d) ve sebep belirtmelisin!`').then(x => x.delete({ timeout: 3000 }));
-        if (message.member.roles.highest.position <= üye.roles.highest.position) return message.lineReply('`Etiketlediğin kullanıcı senden üst veya senle aynı pozisyonda!`').then(x => x.delete({ timeout: 3000 }))
+        if (message.member.roles.highest.position <= üye.roles.highest.position) return message.reply('`Etiketlediğin kullanıcı senden üst veya senle aynı pozisyonda!`').then(x => x.delete({ timeout: 3000 }))
 
         db.push(`üye.${üye.id}.ssicil`, { Yetkili: message.author.id, Tip: "VOICE MUTE", Sebep: sebep, Zaman: Date.now(), Süre: süre });
 
         client.channels.cache.get(id.Mute.mutelogkanalid).send(new Discord.MessageEmbed().setColor('#97ffff').setDescription(`${üye}\`(${üye.id})\` adlı üye, <@${message.author.id}>\`(${message.author.id})\` üyesi tarafından \`(${new Date().toTurkishFormatDate()})\` zamanında \`(${sebep})\` sebebiyle \`${süre}\` boyunca ses kanallarında susturuldu.`))
 
-        üye.voice.setMute(true), message.lineReply(`\`Etiketlenen üye başarıyla ses kanallarında (${süre}) boyunca susturuldu!\``).then(x => x.delete({ timeout: 9000 }), message.react(id.Emojiler.başarılıemojiid))
+        üye.voice.setMute(true), message.reply(`\`Etiketlenen üye başarıyla ses kanallarında (${süre}) boyunca susturuldu!\``).then(x => x.delete({ timeout: 9000 }), message.react(id.Emojiler.başarılıemojiid))
 
         setTimeout(function () {
             üye.voice.setMute(false)
