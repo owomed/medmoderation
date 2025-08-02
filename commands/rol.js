815174a141ea5b7992f954961ae0e62ccf6b71f3
owomed <owomedsunucu@gmail.com>
2025-08-02 22:55:37 +0300

    rol.js

--- commands/rol.js
+++ commands/rol.js
@@ -14,14 +14,14 @@ module.exports = {
         let üye = message.guild.member(message.mentions.users.first()) || message.guild.members.cache.get(args[1]);
         let rol = message.mentions.roles.first() || message.guild.roles.cache.get(args[2]);
 
-        if (!üye || !rol) return message.lineReply('`Lütfen geçerli bir üye ve rol belirtin!`').then(x => x.delete({ timeout: 3000 }));
+        if (!üye || !rol) return message.reply('`Lütfen geçerli bir üye ve rol belirtin!`').then(x => x.delete({ timeout: 3000 }));
 
         if (message.member.roles.highest.position <= üye.roles.highest.position) return message.lineReply('`Etiketlediğin üye veya rol senden üst veya senle aynı pozisyonda!`').then(x => x.delete({ timeout: 3000 }));
 
         if (args[0] === "al") {
             if (!üye.roles.cache.has(rol.id)) return message.lineReply('`Etiketlenen üyede etiketlenen rol bulunmamaktadır!`').then(x => x.delete({ timeout: 3000 }));
             üye.roles.remove(rol).then(() => {
-                message.lineReply('`Başarıyla etiketlenen üyeden rolü aldım.`').then(x => x.delete({ timeout: 7 * 1000 }), message.react(id.Emojiler.başarılıemojiid));
+                message.reply('`Başarıyla etiketlenen üyeden rolü aldım.`').then(x => x.delete({ timeout: 7 * 1000 }), message.react(id.Emojiler.başarılıemojiid));
             });
             return;
         };
@@ -29,11 +29,11 @@ module.exports = {
         if (args[0] === "ver") {
             if (üye.roles.cache.has(rol.id)) return message.lineReply('`Etiketlenen üyede etiketlenen rol bulunmaktadır!`').then(x => x.delete({ timeout: 3000 }));
             üye.roles.add(rol).then(() => {
-                message.lineReply('`Başarıyla etiketlenen üyeye rol verdim.`').then(x => x.delete({ timeout: 7 * 1000 }), message.react(id.Emojiler.başarılıemojiid));
+                message.reply('`Başarıyla etiketlenen üyeye rol verdim.`').then(x => x.delete({ timeout: 7 * 1000 }), message.react(id.Emojiler.başarılıemojiid));
             });
             return;
         };
 
-        message.lineReply('`Ver veya al argümanlarını kullanmalısın!`').then(x => x.delete({ timeout: 3000 }));
+        message.reply('`Ver veya al argümanlarını kullanmalısın!`').then(x => x.delete({ timeout: 3000 }));
     }
 }
