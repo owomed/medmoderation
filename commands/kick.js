518e09cad39029d3ba1f1851aedd695f25748beb
owomed <owomedsunucu@gmail.com>
2025-08-02 22:54:12 +0300

    kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -8,26 +8,26 @@ module.exports = {
     aliases: [],
     async execute(client, message, args) {
         if (!message.member.hasPermission('KICK_MEMBERS') && !message.member.roles.cache.some(role => id.Kick.kickyetkiliid.includes(role.id)) && message.author.id !== ayar.sahip) {
-            return message.lineReply('`Bu komudu kullanmak için gerekli izinlere sahip değilsin!`').then(x => x.delete({ timeout: 3000 }), message.react(id.Emojiler.başarısızemojiid));
+            return message.reply('`Bu komudu kullanmak için gerekli izinlere sahip değilsin!`').then(x => x.delete({ timeout: 3000 }), message.react(id.Emojiler.başarısızemojiid));
         }
 
         if (!message.guild.me.hasPermission('KICK_MEMBERS')) {
-            return message.lineReply('`Botun bu komudu kullanmak için gerekli izinlere sahip değil!`').then(x => x.delete({ timeout: 3000 }));
+            return message.reply('`Botun bu komudu kullanmak için gerekli izinlere sahip değil!`').then(x => x.delete({ timeout: 3000 }));
         }
 
         let üye = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
         let sebep = args.slice(1).join(' ');
 
         if (!üye || !sebep) {
-            return message.lineReply('`Sunucudan atabilmek için üye ve sebep belirtmelisin!`').then(x => x.delete({ timeout: 3000 }));
+            return message.reply('`Sunucudan atabilmek için üye ve sebep belirtmelisin!`').then(x => x.delete({ timeout: 3000 }));
         }
         
         if (!sebep.trim()) {
-            return message.lineReply('`Geçerli bir atılma sebebi belirtmelisin!`').then(x => x.delete({ timeout: 3000 }));
+            return message.reply('`Geçerli bir atılma sebebi belirtmelisin!`').then(x => x.delete({ timeout: 3000 }));
         }
 
         if (message.member.roles.highest.position <= üye.roles.highest.position) {
-            return message.lineReply('`Etiketlediğin kullanıcı senden üst veya senle aynı pozisyonda!`').then(x => x.delete({ timeout: 3000 }));
+            return message.reply('`Etiketlediğin kullanıcı senden üst veya senle aynı pozisyonda!`').then(x => x.delete({ timeout: 3000 }));
         }
 
         db.push(`üye.${üye.id}.sicil`, { Yetkili: message.author.id, Tip: "KICK", Sebep: sebep, Zaman: Date.now() });
@@ -47,7 +47,7 @@ module.exports = {
                 .addField('Komut', `-kick ${üye.id}`, true);
 
             // Kanala embed mesaj gönder
-            await message.lineReply(embed).then(() => message.react(id.Emojiler.başarılıemojiid)).catch(err => {
+            await message.reply(embed).then(() => message.react(id.Emojiler.başarılıemojiid)).catch(err => {
                 console.error('Embed mesajı gönderilirken bir hata oluştu:', err);
             });
 
@@ -60,7 +60,7 @@ module.exports = {
             }
         } catch (error) {
             console.error('Kick işlemi sırasında bir hata oluştu:', error);
-            message.lineReply('`Kick işlemi sırasında bir hata oluştu!`').then(x => x.delete({ timeout: 9000 }), message.react(id.Emojiler.başarısızemojiid));
+            message.reply('`Kick işlemi sırasında bir hata oluştu!`').then(x => x.delete({ timeout: 9000 }), message.react(id.Emojiler.başarısızemojiid));
         }
     }
 };
