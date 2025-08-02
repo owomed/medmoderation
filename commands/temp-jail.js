c5d5a74d5a6223e970a956826d34b6d11eaa68bd
owomed <owomedsunucu@gmail.com>
2025-08-02 23:05:42 +0300

    temp-jail.js

--- commands/temp-jail.js
+++ commands/temp-jail.js
@@ -16,9 +16,9 @@ module.exports = {
         let üye = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
         let sebep = args.slice(2).join(' ');
         let süre = args[1];
-        if (!üye || !süre || !sebep) return message.lineReply('`Jaile atmak için üye, süre(1s,1m,1h,1d) ve sebep belirtmelisin!`').then(x => x.delete({ timeout: 3000 }));
-        if (üye.roles.cache.get(id.Jail.jailrolid)) return message.lineReply('`Etiketlenen üye zaten jailde!`').then(x => x.delete({ timeout: 3000 }));
-        if (message.member.roles.highest.position <= üye.roles.highest.position) return message.lineReply('`Etiketlediğin kullanıcı senden üst veya senle aynı pozisyonda!`').then(x => x.delete({ timeout: 3000 }))
+        if (!üye || !süre || !sebep) return message.reply('`Jaile atmak için üye, süre(1s,1m,1h,1d) ve sebep belirtmelisin!`').then(x => x.delete({ timeout: 3000 }));
+        if (üye.roles.cache.get(id.Jail.jailrolid)) return message.reply('`Etiketlenen üye zaten jailde!`').then(x => x.delete({ timeout: 3000 }));
+        if (message.member.roles.highest.position <= üye.roles.highest.position) return message.reply('`Etiketlediğin kullanıcı senden üst veya senle aynı pozisyonda!`').then(x => x.delete({ timeout: 3000 }))
 
         let rols = üye.roles.cache
 
@@ -26,7 +26,7 @@ module.exports = {
 
         client.channels.cache.get(id.Jail.jaillogkanalid).send(new Discord.MessageEmbed().setColor('#00ff66').setDescription(`${üye}\`(${üye.id})\` adlı üye, <@${message.author.id}>\`(${message.author.id})\` üyesi tarafından \`(${new Date().toTurkishFormatDate()})\` zamanında \`(${sebep})\` sebebiyle \`${süre}\` boyunca jaile atıldı.`))
 
-        üye.roles.set([id.Jail.jailrolid]), message.lineReply(`\`Etiketlenen üye başarıyla (${süre}) boyunca jaile atıldı!\``).then(x => x.delete({ timeout: 9000 }), message.react(id.Emojiler.başarılıemojiid))
+        üye.roles.set([id.Jail.jailrolid]), message.reply(`\`Etiketlenen üye başarıyla (${süre}) boyunca jaile atıldı!\``).then(x => x.delete({ timeout: 9000 }), message.react(id.Emojiler.başarılıemojiid))
 
         setTimeout(function () {
             if (üye.roles.cache.get(id.Jail.jailrolid)) {
